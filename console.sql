b588c156b8bc75a051f2c5468afa802466c456f5
hoalandodat-png <hoalandodat@gmail.com>
2026-08-12 10:06:15 +0700

    Add files via upload

--- console.sql
+++ console.sql
@@ -1,1 +1,11 @@
+-- Tạo database
+CREATE DATABASE rikkei_erp_db
+    WITH
+    OWNER     = postgres
+    ENCODING  = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE   = 'en_US.UTF-8'
+    TEMPLATE  = template0;
 
+-- Kết nối vào database vừa tạo
+\c rikkei_erp_db;
